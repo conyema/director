b8ca2caf1d67afe582dc54110dccc2e11d6b59f3
chinedum onyema <onyemachinedum@gmail.com>
2020-01-19 11:15:18 +0100

    feat: add server file

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const request = require('request');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/currencies', (req, res) => {
+  request(
+    { url: 'http://apilayer.net/api/live?access_key=4f43e92e9d68749f75c7cd58f101eae1' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  )
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
